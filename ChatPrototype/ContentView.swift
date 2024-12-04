fc6b5ec3ddbb6805b23b00a2421d7df82d669150
Neil Shah <nshah@lcs.on.ca>
2024-12-04 16:39:06 -0500

    Added 6 text boxes for descriptions and 1 for a title. Text was put into 3 HStacks in a VStack to make 3 rows , 2 columns of texts. Added padding, shadows, and backgrounds to each description text.

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -9,20 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (spacing: 10){
-            Text("Knock, knock!")
-                .padding()
-                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 10))
-                .shadow(color: Color.mint, radius: 50)
-            
-            Text("Who's there?")
-                .padding()
-                .background(Color.mint, in: RoundedRectangle(cornerRadius: 10))
-                .shadow(color: Color.yellow, radius: 50)
+        VStack {
+            Text("Myself in six words")
+                .font(.system(.largeTitle))
+            HStack {
+                Text("Perseverant")
+                    .padding()
+                    .background(Color.yellow, in: RoundedRectangle(cornerRadius: 10))
+                    .shadow(color: Color.purple, radius: 50)
+                Text("Independent")
+                    .padding()
+                    .background(Color.purple, in: RoundedRectangle(cornerRadius: 10))
+                    .shadow(color: Color.yellow, radius: 50)
+            }
+            HStack {
+                Text("Consistent")
+                    .padding()
+                    .background(Color.orange, in: RoundedRectangle(cornerRadius: 10))
+                    .shadow(color: Color.blue, radius: 50)
+                Text("Disciplined")
+                    .padding()
+                    .background(Color.blue, in: RoundedRectangle(cornerRadius: 10))
+                    .shadow(color: Color.orange, radius: 50)
+            }
+            HStack {
+                Text("Smart")
+                    .padding()
+                    .background(Color.red, in: RoundedRectangle(cornerRadius: 10))
+                    .shadow(color: Color.green, radius: 50)
+                Text("Quick")
+                    .padding()
+                    .background(Color.green, in: RoundedRectangle(cornerRadius: 10))
+                    .shadow(color: Color.red, radius: 50)
+            }
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
